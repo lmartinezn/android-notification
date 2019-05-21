5d635100e2feff2fa6a4b12037b0dc820bdcaac0
lmartinezn <lmartinezn@edu.tecnocampus.cat>
2019-05-21 09:56:46 +0200

    Added ability to send notifications

--- app/src/main/java/cat/tecnocampus/mobileapps/basicnotificationtest/MainActivity.java
+++ app/src/main/java/cat/tecnocampus/mobileapps/basicnotificationtest/MainActivity.java
@@ -2,9 +2,14 @@ package cat.tecnocampus.mobileapps.basicnotificationtest;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -27,8 +32,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void createNotificationChannel(){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){ //API >= 26 (Android O)
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { //API >= 26 (Android O)
             String tag = "TEST NOTIS";
 
             NotificationChannel channel = new NotificationChannel(
@@ -40,5 +45,39 @@ public class MainActivity extends AppCompatActivity {
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel);
         }
+    }
+
+    private void sendNotification(String content) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        //Configurem pendingIntent a partir de intent ja creat
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                intent,
+                PendingIntent.FLAG_ONE_SHOT
+        );
+
+        //Agafem el so per defecte de les notificacions al nostre sistema operatiu
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        /*Notification builder, compte! Hi ha el NotificationBuilder (api >= 26)
+        /i el NotificationCompat (compatible amb api antiga)*/
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(
+                this,
+                CHANNEL_ID
+        );
+
+        notificationBuilder
+                //Paràmetres obligatoris
+                .setSmallIcon(android.R.drawable.stat_notify_chat)
+                .setContentTitle("Notifications Test Project")
+                .setContentText(content)
+                //Paràmetres opcionals
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent)
+        ;
     }
 }
