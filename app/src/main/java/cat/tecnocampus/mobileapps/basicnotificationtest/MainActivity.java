4e1d04e311b97e60fedac948efaac99b8cd52a04
lmartinezn <lmartinezn@edu.tecnocampus.cat>
2019-05-21 09:47:22 +0200

    Create previously defined notification channel

--- app/src/main/java/cat/tecnocampus/mobileapps/basicnotificationtest/MainActivity.java
+++ app/src/main/java/cat/tecnocampus/mobileapps/basicnotificationtest/MainActivity.java
@@ -36,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
                     tag,
                     NotificationManager.IMPORTANCE_DEFAULT
             );
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
         }
     }
 }
